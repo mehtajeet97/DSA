02e90b2c033e8a0a21b6e4ca75130950e06d8097
Jeet Mehta <mehta.jeet97@gmail.com>
2024-11-06 17:31:33 -0800

    easy problem 392

---
+++ EASY/10_Is_Subsequence/is_subsequence.py
@@ -0,0 +1,10 @@
+# While Loop -- Increment subsequence string s only if match found in string t. Check if length of increment variable same as s
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
