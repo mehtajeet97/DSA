c164bf0201f257a97127dfa2dfeab005debd5233
Jeet Mehta <mehta.jeet97@gmail.com>
2024-11-10 10:09:01 -0800

    easy problem 496

---
+++ EASY/18_Next_Greater_Element_1/next_greater_element_1.py
@@ -0,0 +1,21 @@
+# Use a hashtable for nums1 and a stack
+# Input stack with values from nums1  
+# Pop values if current value is greater than the one in stack
+
+from typing import List
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1idx = {n:i for i,n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        stack =[]
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1idx[val]
+                res[idx] = cur
+            if cur in nums1idx:
+                stack.append(cur)
+        return res
+        
