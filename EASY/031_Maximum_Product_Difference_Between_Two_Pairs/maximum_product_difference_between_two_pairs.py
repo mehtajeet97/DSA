b247d2b178dc6af9e16be19b2b035703f2265f26
Jeet Mehta <mehta.jeet97@gmail.com>
2024-11-13 09:40:16 -0800

    easy folder 31 mod

--- EASY/031_Maximum_Product_Difference_Between_Two_Pairs/maximum_product_difference_between_two_pairs.py
+++ EASY/031_Maximum_Product_Difference_Between_Two_Pairs/maximum_product_difference_between_two_pairs.py
@@ -3,5 +3,5 @@ class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
         # Sort Tha Array
         nums.sort()
-        # Subtract the product of first two(max) and last two(min)
+        # Subtract the product of last two(max) and first two(min)
         return nums[-1] * nums[-2] - nums[0] * nums[1]
