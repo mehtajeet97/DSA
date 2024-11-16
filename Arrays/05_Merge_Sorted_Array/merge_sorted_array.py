1cdeb398e30afe2ad82e0f831757509d22172682
Jeet Mehta <mehta.jeet97@gmail.com>
2024-11-15 20:33:34 -0800

    array folder 5 commented

--- Arrays/05_Merge_Sorted_Array/merge_sorted_array.py
+++ Arrays/05_Merge_Sorted_Array/merge_sorted_array.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # Initiate pointers i, j, k from the end of arrays
         i, j, k = m - 1, n - 1, m + n - 1
         while i >= 0 and j >= 0:
+        #Check for the greater number in two arrays and place it at the end
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
@@ -12,6 +14,7 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
+        # Check for any remaining elements in second array 
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
